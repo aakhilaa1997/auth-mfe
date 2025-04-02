d5155b91ee0f10a2151da45cb1848cd0e994094e
akhilaakula <akhilaadp@gmail.com>
2025-04-01 22:21:12 -0700

    Login,register, forgotpassword and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: LoginComponent,
+    },
+    {
+        path: 'forgot-password', component: ForgotPasswordComponent,
+    },
+    {
+        path:'register', component: RegisterComponent,
+    }
+];
