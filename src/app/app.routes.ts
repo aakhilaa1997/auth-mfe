472b2c3bb4f3b1ed695dfc1aaec31c9d7b1aa0c9
akhilaakula <akhilaadp@gmail.com>
2025-04-02 23:03:41 -0700

    removed import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { RegisterComponent } from './register/register.component';
-import { HomescreenComponent } from './homescreen/homescreen.component';
 
 export const routes: Routes = [
     {
