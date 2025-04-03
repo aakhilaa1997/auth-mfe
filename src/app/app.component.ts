47fd21abf693c38411d2176c0fee726cd94958bf
akhilaakula <akhilaadp@gmail.com>
2025-04-02 22:30:03 -0700

    homescreen dropdown options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet ],
+  imports: [RouterOutlet , CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'one-shop-AI';
+  showDropdown = true;
   constructor(private router: Router){
+     // Listen to route changes and toggle dropdown visibility
+      this.router.events.subscribe(() => {
+      this.showDropdown = this.router.url == '/homescreen'; // Hide dropdown on the login page
+    });
   }
+
 
 navigateToHome() {
   this.router.navigate(['/homescreen']); // Navigates to the home route
