8ae8708222050b65d47d5e6b171bf11930d03302
akhilaakula <akhilaadp@gmail.com>
2025-04-05 00:53:27 -0700

    Styling of all pages is done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,5 +12,14 @@ export const routes: Routes = [
     },
     {
         path:'register', component: RegisterComponent,
+    },
+    {
+        // Redirect to login if no path is provided
+        // This is useful for when the user tries to access the app without a path
+        // or when the user is not logged in and tries to access a protected route
+        // You can also use a guard to protect the route
+        // and redirect to login if the user is not logged in
+        // this is default redirect
+        path:'', redirectTo: 'login', pathMatch: 'full'
     }
 ];
