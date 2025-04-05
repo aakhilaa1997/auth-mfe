6fc0cd4b7305c630f4af2902c96ae874b778f813
akhilaakula <akhilaadp@gmail.com>
2025-04-04 22:41:39 -0700

    added favicon, removed app component files, updated main.ts file, styled login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,19 +2,14 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 
-
-
 @Component({
   selector: 'app-login',
   imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
+
 })
 export class LoginComponent {
   constructor(private router: Router){
   }
-
-navigateToHome() {
-  this.router.navigate(['/homescreen']); // Navigates to the home route
-}
 }
